00be7677471fd3f19054344e8490e55d9cd8d54e
Paul Ryan <paul@sharpred.com>
2011-06-17 16:26:22 +0100

    initial checkin

---
+++ ConsoleApplicationDrupal/DrupalForm.vb
@@ -0,0 +1,25 @@
+﻿Imports Microsoft.Http
+
+Public Class DrupalForm
+    Public Function create(ByVal type As String, ByVal formatoutput As Boolean) As HttpMultipartMimeForm
+        Dim form As New HttpMultipartMimeForm
+        Try
+            Select Case type
+                Case "node"
+                Case "menu"
+                Case Else
+                    'gets treated as a view if not specified
+                    form.Add("method", My.Settings.service)
+                    form.Add(My.Settings.serviceParameter, My.Settings.serviceValue)
+            End Select
+            If formatoutput = True Then
+                form.Add("format_output", "true")
+            End If
+
+        Catch ex As Exception
+
+        End Try
+        Return form
+    End Function
+
+End Class
---
+++ ConsoleApplicationDrupal/Module1.vb
@@ -0,0 +1,39 @@
+﻿Imports Microsoft.Http
+Imports Microsoft.Http.HttpClient
+Imports Microsoft.Http.HttpMethodExtensions
+Imports Newtonsoft.Json
+Imports Newtonsoft.Json.Converters
+Imports System.Net
+Imports System.Xml
+Imports System.IO
+Imports System.Text
+Module Module1
+    Dim output As String
+    Sub Main()
+        Try
+            Dim http As New HttpClient(My.Settings.myURL)
+            Dim drupalform As New DrupalForm
+            Dim form As HttpMultipartMimeForm = drupalform.create("view", False)
+            Dim response As HttpResponseMessage = http.Post("/services/json", form.CreateHttpContent)
+            response.EnsureStatusIsSuccessful()
+            output = response.Content.ReadAsString
+            Dim sw As New StreamWriter("node_out.json")
+
+            'uncomment these to lose the crappiness at the start of the json output
+            'REMEMBER to write out newoutput to file instead of output
+            'Dim replacementstring As String = "#error" & """: false, """ & "#data"
+            'Dim newoutput As String = output.Replace(replacementstring, "content")
+            Console.WriteLine(output)
+            sw.Write(output)
+            sw.Flush()
+            sw.Close()
+
+        Catch ex As Exception
+
+            Console.WriteLine(ex.Message & " " & output)
+        End Try
+ 
+    End Sub
+
+
+End Module
